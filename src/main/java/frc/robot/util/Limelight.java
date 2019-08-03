83b36e26fb75277e81203ea83af8799899bf7a0f
20jatink <20jatink@students.harker.org>
2019-08-03 13:50:14 -0700

    Change Limelight from Singleton to have all static methods

--- src/main/java/frc/robot/util/Limelight.java
+++ src/main/java/frc/robot/util/Limelight.java
@@ -1,7 +1,5 @@
 package frc.robot.util;
 
-import java.util.function.Function;
-
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 
@@ -13,11 +11,9 @@ import edu.wpi.first.networktables.NetworkTableInstance;
  * @since 1/12/19
  */
 public class Limelight {
-    private static Limelight instance;
-
     public static final String LIMELIGHT_TABLE_KEY = "limelight";
-    public final NetworkTable table = NetworkTableInstance.getDefault().getTable(LIMELIGHT_TABLE_KEY);
-    
+    public static final NetworkTable table = NetworkTableInstance.getDefault().getTable(LIMELIGHT_TABLE_KEY);
+
     public static final String TV_KEY = "tv";
     public static final String TX_KEY = "tx";
     public static final String TY_KEY = "ty";
@@ -42,17 +38,19 @@ public class Limelight {
 
     public static final int NO_SNAPSHOT = 0;
     public static final int SNAPSHOT = 1;
- 
-    public static double THOR_EXPONENT = 0.62291;
-    public static double THOR_COEFF = 1000;
+
+    // public static double THOR_EXPONENT = 0.62291;
+    // public static double THOR_COEFF = 1000;
     public static double TX_SETPOINT = 0;
-    public static Function<Double, Double> THOR_LINEARIZATION_FUNCTION = (thor) -> (THOR_COEFF / Math.pow(thor, THOR_EXPONENT));
+    // public static Function<Double, Double> THOR_LINEARIZATION_FUNCTION = (thor)
+    // -> (THOR_COEFF / Math.pow(thor, THOR_EXPONENT));
+
+    private static double[] nullArr;
 
     /**
      * Setup Limelight with default settings
      */
-    private Limelight()
-    {
+    private Limelight() {
         table.getEntry(MODE_KEY).setNumber(VISION_MODE);
         table.getEntry(SNAP_KEY).setNumber(NO_SNAPSHOT);
     }
@@ -60,49 +58,47 @@ public class Limelight {
     /**
      * Determines whether a target has been latched onto.
      * 
-     * @return true if a target is visible; otherwise,
-     *         false
+     * @return true if a target is visible; otherwise, false
      */
-    public boolean isTargetVisible() {
+    public static boolean isTargetVisible() {
         return Math.abs(table.getEntry(TV_KEY).getDouble(0.0) - 1.0) < 1e-5;
     }
 
     /**
-     * Determines the horizontal angular distance from the crosshair to the center of the bounding box representing the target.
+     * Determines the horizontal angular distance from the crosshair to the center
+     * of the bounding box representing the target.
      * 
      * @return the horizontal angular distance to the target, in degrees
      */
-    public double getTx() {
+    public static double getTx() {
         return table.getEntry(TX_KEY).getDouble(0.0);
     }
 
     /**
      * Toggles the limelight between driver and vision mode
      */
-    public void toggleCamMode()
-    {
+    public static void toggleCamMode() {
         if (table.getEntry(MODE_KEY).getDouble(0) == VISION_MODE)
             table.getEntry(MODE_KEY).setNumber(DRIVER_MODE);
         else
             table.getEntry(MODE_KEY).setNumber(VISION_MODE);
     }
 
-    public void setCamModeDriver()
-    {
+    public static void setCamModeDriver() {
         table.getEntry(MODE_KEY).setNumber(DRIVER_MODE);
     }
 
-    public void setCamModeVision()
-    {
+    public static void setCamModeVision() {
         table.getEntry(MODE_KEY).setNumber(VISION_MODE);
     }
 
     /**
-     * Determines the vertical angular distance from the crosshair to the center of the bounding box representing the target.
+     * Determines the vertical angular distance from the crosshair to the center of
+     * the bounding box representing the target.
      * 
      * @return the vertical, angular distance to the target, in degrees
      */
-    public double getTy() {
+    public static double getTy() {
         return table.getEntry(TY_KEY).getDouble(0.0);
     }
 
@@ -111,7 +107,7 @@ public class Limelight {
      * 
      * @return the angular skew, in degrees [-90, 0]
      */
-    public double getTs() {
+    public static double getTs() {
         return table.getEntry(TS_KEY).getDouble(0.0);
     }
 
@@ -120,56 +116,65 @@ public class Limelight {
      * 
      * @return the area, as a percent of the total screen
      */
-    public double getTa() {
+    public static double getTa() {
         return table.getEntry(TA_KEY).getDouble(0.0);
     }
 
     /**
-     * Determines the latency of the camera feed, or a measure of how long the feed takes to process.
+     * Determines the latency of the camera feed, or a measure of how long the feed
+     * takes to process.
      * 
-     * @return the latency, in milliseconds 
+     * @return the latency, in milliseconds
      */
-    public double getTl() {
+    public static double getTl() {
         return table.getEntry(TL_KEY).getDouble(0.0);
     }
 
     /**
      * Determines the sidelength of the shortest side of the fitted bounding box.
-     * The fitted bounding box is a rectangular convex hull surrounding the selected pixels,
-     * while the rough bounding box is a rectangle around the fitted bounding box, put is not rotated
+     * The fitted bounding box is a rectangular convex hull surrounding the selected
+     * pixels, while the rough bounding box is a rectangle around the fitted
+     * bounding box, put is not rotated
+     * 
      * @return the shortest sidelength, in pixels
      */
-    public double getTshort() {
+    public static double getTshort() {
         return table.getEntry(TSHORT_KEY).getDouble(0.0);
     }
 
     /**
-     * Determines the sidelength of the longest side of the fitted bounding box.
-     * The fitted bounding box is a rectangular convex hull surrounding the selected pixels,
-     * while the rough bounding box is a rectangle around the fitted bounding box, put is not rotated
+     * Determines the sidelength of the longest side of the fitted bounding box. The
+     * fitted bounding box is a rectangular convex hull surrounding the selected
+     * pixels, while the rough bounding box is a rectangle around the fitted
+     * bounding box, put is not rotated
+     * 
      * @return the longest sidelength, in pixels
      */
-    public double getTlong() {
+    public static double getTlong() {
         return table.getEntry(TLONG_KEY).getDouble(0.0);
     }
 
     /**
-     * Determines the horizontal sidelength of the rough bounding box.
-     * The fitted bounding box is a rectangular convex hull surrounding the selected pixels,
-     * while the rough bounding box is a rectangle around the fitted bounding box, put is not rotated
+     * Determines the horizontal sidelength of the rough bounding box. The fitted
+     * bounding box is a rectangular convex hull surrounding the selected pixels,
+     * while the rough bounding box is a rectangle around the fitted bounding box,
+     * put is not rotated
+     * 
      * @return the horizontal sidelength, in pixels [0, 320]
-     */    
-    public double getThor() {
+     */
+    public static double getThor() {
         return table.getEntry(THOR_KEY).getDouble(0.0);
     }
 
     /**
-     * Determines the vertical sidelength of the rough bounding box.
-     * The fitted bounding box is a rectangular convex hull surrounding the selected pixels,
-     * while the rough bounding box is a rectangle around the fitted bounding box, put is not rotated
+     * Determines the vertical sidelength of the rough bounding box. The fitted
+     * bounding box is a rectangular convex hull surrounding the selected pixels,
+     * while the rough bounding box is a rectangle around the fitted bounding box,
+     * put is not rotated
+     * 
      * @return the vertical sidelength, in pixels [0, 240]
      */
-    public double getTvert() {
+    public static double getTvert() {
         return table.getEntry(TVERT_KEY).getDouble(0.0);
     }
 
@@ -177,12 +182,10 @@ public class Limelight {
      * Returns the values of the Limelight's compute 3D localization calculations.
      * 
      * @return The compute 3D values, in the specified order (x,y,z,pitch,yaw,roll).
-     *         These values place the target as the origin and represent the camera's
-     *         orientation relative to the target.
+     *         These values place the target as the origin and represent the
+     *         camera's orientation relative to the target.
      */
-    public double[] getCamtranData()
-    {
-        double[] nullArr = null;
+    public static double[] getCamtranData() {
         return table.getEntry(CAMTRAN_KEY).getDoubleArray(nullArr);
     }
 
@@ -191,18 +194,17 @@ public class Limelight {
      * 
      * @return The camera's horizontal distance from the target.
      */
-    public double getCamtranX()
-    {
+    public static double getCamtranX() {
         return getCamtranData()[0];
     }
 
     /**
      * Returns height difference between the the target
      * 
-     * @return The camera's height relative to the target's height (automatically 0).
+     * @return The camera's height relative to the target's height (automatically
+     *         0).
      */
-    public double getCamtranY()
-    {
+    public static double getCamtranY() {
         return getCamtranData()[1];
     }
 
@@ -211,8 +213,7 @@ public class Limelight {
      * 
      * @return The camera's vertical distance from the target.
      */
-    public double getCamtranZ()
-    {
+    public static double getCamtranZ() {
         return getCamtranData()[2];
     }
 
@@ -221,8 +222,7 @@ public class Limelight {
      * 
      * @return The camera's pitch relative to the target (automatically 0).
      */
-    public double getCamtranPitch()
-    {
+    public static double getCamtranPitch() {
         return getCamtranData()[3];
     }
 
@@ -231,8 +231,7 @@ public class Limelight {
      * 
      * @return The camera's pitch relative to the target (automatically 0).
      */
-    public double getCamtranYaw()
-    {
+    public static double getCamtranYaw() {
         return getCamtranData()[4];
     }
 
@@ -241,54 +240,39 @@ public class Limelight {
      * 
      * @return The camera's pitch relative to the target (automatically 0).
      */
-    public double getCamtranRoll()
-    {
+    public static double getCamtranRoll() {
         return getCamtranData()[5];
     }
 
-    public double getRawContourTx(int contourId) {
+    public static double getRawContourTx(int contourId) {
         return table.getEntry(TX_KEY + contourId).getDouble(0.0);
     }
 
-    public double getRawContourTy(int contourId) {
+    public static double getRawContourTy(int contourId) {
         return table.getEntry(TY_KEY + contourId).getDouble(0.0);
     }
 
-    public double getRawContourTa(int contourId) {
+    public static double getRawContourTa(int contourId) {
         return table.getEntry(TA_KEY + contourId).getDouble(0.0);
     }
 
-    public double getRawContourTs(int contourId) {
+    public static double getRawContourTs(int contourId) {
         return table.getEntry(TS_KEY + contourId).getDouble(0.0);
     }
 
-    public double[] getCornersX() {
-        double[] nullArr = null;
+    public static double[] getCornersX() {
         return table.getEntry(CORNERX_KEY).getDoubleArray(nullArr);
     }
 
-    public double[] getCornersY() {
-        double[] nullArr = null;
+    public static double[] getCornersY() {
         return table.getEntry(CORNERY_KEY).getDoubleArray(nullArr);
     }
 
-    public double getLeftArea () {
-        return getRawContourTx(0) <  getRawContourTx(1) ? getRawContourTa(0) : getRawContourTa(1);
+    public static double getLeftArea() {
+        return getRawContourTx(0) < getRawContourTx(1) ? getRawContourTa(0) : getRawContourTa(1);
     }
 
-    public double getRightArea() {
-        return getRawContourTx(0) >  getRawContourTx(1) ? getRawContourTa(0) : getRawContourTa(1);
-    }
-
-    /**
-     * Gets the singleton instance of the limelight class.
-     * 
-     * @return the instance of the limelight class
-     */
-    public static Limelight getInstance() {
-        if(instance == null) {
-            instance = new Limelight();
-        }
-        return instance;
+    public static double getRightArea() {
+        return getRawContourTx(0) > getRawContourTx(1) ? getRawContourTa(0) : getRawContourTa(1);
     }
 }
